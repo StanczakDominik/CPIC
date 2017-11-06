29c3a07148f010951bf18d1ea6e44c26022a5d39
Dominik Stanczak <stanczakdominik@gmail.com>
2017-11-06 18:05:59 +0100

    add parameters for species

--- species.cpp
+++ species.cpp
@@ -14,19 +14,31 @@ class Species
         float eff_m;
         ArrayXd x;
         ArrayX3d v;
-    Species(int N);
+    Species(int N, float q, float m, float scaling);
+    ArrayXd gather_density();
 };
 
-Species::Species(int N)
+Species::Species(int _N, float _q, float _m, float _scaling)
 {
+    // compute effective charges and masses of macroparticles
+    N = _N;
+    q = _q;
+    m = _m;
+    scaling = _scaling;
+    eff_q = q * scaling;
+    eff_m = m * scaling;
+    // allocate position and velocity arrays
     x = ArrayXd(N);
     v = ArrayX3d(N, 3);
 }
 
 int main()
 {
-    Species s = Species(3);
+    Species s = Species(3, 1, 1, 10.0);
     s.x << 1.5, 3.5, 6.5;
+
     cout << s.x.mean() << endl;
+    cout << s.q << endl;
+    cout << s.v.size() << endl;
 }
 
